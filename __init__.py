68135d8e883ef6e138f7e4363cb9cf4f67c12713
pgming <96xgsatw5i@gmail.com>
2021-07-02 07:56:25 +0900

    add the first source

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from . import fwmodule_general
+from . import fwmodule_console_progress
+from . import fwmodule_gui_progress
+from . import module_console_text
+from . import module_gui_text
+from . import module_bit
