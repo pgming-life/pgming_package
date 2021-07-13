7cb800cfa335091db16a0ea0d05a5081aafe2754
Pgming <84230279+pgming-life@users.noreply.github.com>
2021-07-13 14:24:27 +0900

    Update comments

--- fwmodule_general.py
+++ fwmodule_general.py
@@ -5,29 +5,24 @@ flag_release = False
 try:
     import pgming_package.release as r
 except Exception:
-    # エラースルー
     pass
 else:
     flag_release = r.flag
 
-# 上記のインポートが読み込めない場合はflag_releaseは基本的にFalse(存在しない変数であるため)
-if flag_release:    # リリース用インポート
+if flag_release:
     from pgming_package.fwdef import *
-else:               # デバッグ用インポート
-    # ルートパス可変制御
-    import sys, os      # fwdef.pyと同じモジュールインポートだが、Python内部でインクルードガード出来ているので問題ない。
+else:
+    # variable route path control for VSCode debug
+    import sys, os
     if not [i for i in sys.path if i == os.path.dirname(__file__)]:
         sys.path.append(os.path.dirname(__file__))
     from fwdef import *
 
-#####################################################################
 """
-    カウント
-    備考:
-    ・呼び出すごとにカウントする
-    ・クラスなので定義するときに分けることが可能
-    ・resultでそれまでの結果を返す
-
+    Counter
+    details:
+    ・Count each the count().
+    ・Return the previous result with the result().
 """
 class counter:
     def __init__(self, radix=0, operand=1, operator="+"):
@@ -55,15 +50,12 @@ class counter:
     def result(self):
         return self.radix
 
-#####################################################################
 """
-    テスト
-
+    Tests
 """
-# ↓↓↓直接このファイルを実行したときの処理
 if __name__ == "__main__":
     #from fwmodule_general import *
-
+    
     a = counter()
     x = 2
     b = counter(x, 2, "*")
@@ -73,9 +65,7 @@ if __name__ == "__main__":
         b.count()
         c.count()
     print(a.result())
-    print(b.result())       # 1024にはならず、べき乗を狙っても1回多い値になる
-    print(int(b.result() / x))  # なので一回分減らす
+    print(b.result())           # it is not 1024
+    print(int(b.result() / x))  # reduce by one dose
     print(c.result())
-
-    input(">>>\n>>>\n>>>\n処理が終了しました。\n")
     
