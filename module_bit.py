8e8c9348b84055d6278e23e7e10b9b3b46e0f0ab
Pgming <84230279+pgming-life@users.noreply.github.com>
2021-07-13 21:32:19 +0900

    Update comments

--- module_bit.py
+++ module_bit.py
@@ -1,108 +1,106 @@
 """module_bit.py"""
 
-#####################################################################
 """
-    2進数、8進数、10進数、16進数の相互変換
-    備考:
-        ・baseへの入力はradixに乗っ取った変換したいものでint型(10進数以外では0b, 0o, 0x表記必須)
-        ・radix(基数)とoperand(変換対象)への入力はint型(初期値: radix=16進数 operand=2進数)
-        ・flag_printがTrueの場合print出力する
-        ・print出力結果は進数表記となる(戻り値は文字列なのでintでキャストする)
-    ex) number_conv(base=0b10100, radix=2, operand=16) = 0x14
-
+    Bin, Oct, Dec, Hex mutual conversion
+    details:
+    ・base: radix number (other than Decimal: 0b, 0o, 0x)
+    ・radix: 2 | 8 | 10 | 16 (int)
+    ・operand: conversion destination (int)
+    ・flag_print=True: result print
+    ※Return int
+    ex) number_conv(base=0b10100, radix=2, operand=16) = 0x14 (return 20(int))
 """
 def number_conv(base, radix=16, operand=2, flag_print=False):
     if radix == 10:
         if operand == 2:
             num = bin(base)
             if flag_print:
-                print("10進数→2進数: " + num)
+                print("dec→bin: " + num)
             return int(num, 0)
         elif operand == 8:
             num = oct(base)
             if flag_print:
-                print("10進数→8進数: " + num)
+                print("dec→oct: " + num)
             return int(num, 0)
         elif operand == 16:
             num = hex(base)
             if flag_print:
-                print("10進数→16進数: " + num)
+                print("dec→hex: " + num)
             return int(num, 0)
         else:
             if flag_print:
-                print("オペランドが間違っています。\nやり直してください。")
+                print("The operand is incorrect.\nPlease try again.")
     elif radix == 2:
         if operand == 10:
             num = base
             if flag_print:
-                print("2進数→10進数: " + "{}".format(num))
+                print("bin→dec: " + "{}".format(num))
             return num
         elif operand == 8:
             num = oct(base)
             if flag_print:
-                print("2進数→8進数: " + "{}".format(num))
+                print("bin→oct: " + "{}".format(num))
             return int(num, 0)
         elif operand == 16:
             num = hex(base)
             if flag_print:
-                print("2進数→16進数: " + "{}".format(num))
+                print("bin→hex: " + "{}".format(num))
             return int(num, 0)
         else:
             if flag_print:
-                print("オペランドが間違っています。\nやり直してください。")
+                print("The operand is incorrect.\nPlease try again.")
     elif radix == 8:
         if operand == 10:
             num = base
             if flag_print:
-                print("8進数→10進数: " + "{}".format(num))
+                print("oct→dec: " + "{}".format(num))
             return num
         elif operand == 2:
             num = bin(base)
             if flag_print:
-                print("8進数→2進数: " + "{}".format(num))
+                print("oct→bin: " + "{}".format(num))
             return int(num, 0)
         elif operand == 16:
             num = hex(base)
             if flag_print:
-                print("8進数→16進数: " + "{}".format(num))
+                print("oct→hex: " + "{}".format(num))
             return int(num, 0)
         else:
             if flag_print:
-                print("オペランドが間違っています。\nやり直してください。")
+                print("The operand is incorrect.\nPlease try again.")
     elif radix == 16:
         if operand == 10:
             num = base
             if flag_print:
-                print("16進数→10進数: " + "{}".format(num))
+                print("hex→dec: " + "{}".format(num))
             return num
         elif operand == 2:
             num = bin(base)
             if flag_print:
-                print("16進数→2進数: " + "{}".format(num))
+                print("hex→bin: " + "{}".format(num))
             return int(num, 0)
         elif operand == 8:
             num = oct(base)
             if flag_print:
-                print("16進数→8進数: " + "{}".format(num))
+                print("hex→oct: " + "{}".format(num))
             return int(num, 0)
         else:
             if flag_print:
-                print("オペランドが間違っています。\nやり直してください。")
+                print("The operand is incorrect.\nPlease try again.")
     else:
         if flag_print:
-            print("基数が間違っています。\nやり直してください。")
+            print("The radix is incorrect.\nPlease try again.")
 
-#####################################################################
 """
-    ビットシフト
-    備考:
-        ・base(基数)への入力はradixに乗っ取ったシフトしたいものでint型(10進数以外では0b, 0o, 0x表記必須)
-        ・shiftへの入力はint型(いくつシフトするか)
-        ・radix(基数)への入力はbaseで入力する進数型でint型(初期値: 2進数)
-        ・lrへの入力は"left"か"right"のstr型(初期値: 右シフト)
-        ・flag_printがTrueの場合print出力する
-        ・print出力結果は2進数表記となる(戻り値は文字列なのでintでキャストする)
-    ex) bit_shift(base=0b10100, shift=3) = 0b10
+    Bit Shift
+    details:
+    ・base: radix number (other than Decimal: 0b, 0o, 0x)
+    ・shift: int
+    ・radix: 2 | 8 | 10 | 16 (int)
+    ・lr: left | right (str)
+    ・flag_print=True: result print
+    ※Return int
+    ex) bit_shift(base=0b10100, shift=3) = 0b10 (return 2(int))
 
 """
 def bit_shift(base, shift, radix=2, lr="right", flag_print=False):
@@ -111,77 +109,82 @@ def bit_shift(base, shift, radix=2, lr="right", flag_print=False):
         if lr == "left":
             num1 = bin(base << shift)
             if flag_print:
-                print("シフト基数(10進数):   " + num0)
-                print("10進数左シフト(結果): " + num1)
+                print("shift radix (dec):       " + num0)
+                print("dec left shift (result): " + num1)
             return int(num1, 0)
         elif lr == "right":
             num1 = bin(base >> shift)
             if flag_print:
-                print("シフト基数(10進数):   " + num0)
-                print("10進数右シフト(結果): " + num1)
+                print("shift radix (dec):       " + num0)
+                print("dec right shift (result): " + num1)
             return int(num1, 0)
         else:
             if flag_print:
-                print("左シフトか右シフトかの指定がありません。\nやり直してください。")
+                print("There is no specification of left shift or right shift.\nPlease try again.")
     elif radix == 2:
         num0 = bin(base)
         if lr == "left":
             num1 = bin(base << shift)
             if flag_print:
-                print("シフト基数(2進数):   " + "{}".format(num0))
-                print("2進数左シフト(結果): " + "{}".format(num1))
+                print("shift radix (bin):       " + "{}".format(num0))
+                print("bin left shift (result): " + "{}".format(num1))
             return int(num1, 0)
         elif lr == "right":
             num1 = bin(base >> shift)
             if flag_print:
-                print("シフト基数(2進数):   " + "{}".format(num0))
-                print("2進数右シフト(結果): " + "{}".format(num1))
+                print("shift radix (bin):       " + "{}".format(num0))
+                print("bin right shift (result): " + "{}".format(num1))
             return int(num1, 0)
         else:
             if flag_print:
-                print("左シフトか右シフトかの指定がありません。\nやり直してください。")
+                print("There is no specification of left shift or right shift.\nPlease try again.")
     elif radix == 8:
         num0 = bin(base)
         if lr == "left":
             num1 = bin(base << shift)
             if flag_print:
-                print("シフト基数(8進数):   " + "{}".format(num0))
-                print("8進数左シフト(結果): " + "{}".format(num1))
+                print("shift radix (oct):       " + "{}".format(num0))
+                print("oct left shift (result): " + "{}".format(num1))
             return int(num1, 0)
         elif lr == "right":
             num1 = bin(base >> shift)
             if flag_print:
-                print("シフト基数(8進数):   " + "{}".format(num0))
-                print("8進数右シフト(結果): " + "{}".format(num1))
+                print("shift radix (oct):           " + "{}".format(num0))
+                print("oct right shift (result):    " + "{}".format(num1))
             return int(num1, 0)
         else:
             if flag_print:
-                print("左シフトか右シフトかの指定がありません。\nやり直してください。")
+                print("There is no specification of left shift or right shift.\nPlease try again.")
     elif radix == 16:
         num0 = bin(base)
         if lr == "left":
             num1 = bin(base << shift)
             if flag_print:
-                print("シフト基数(16進数):   " + "{}".format(num0))
-                print("16進数左シフト(結果): " + "{}".format(num1))
+                print("shift radix (hex):       " + "{}".format(num0))
+                print("hex left shift (result): " + "{}".format(num1))
             return int(num1, 0)
         elif lr == "right":
             num1 = bin(base >> shift)
             if flag_print:
-                print("シフト基数(16進数):   " + "{}".format(num0))
-                print("16進数右シフト(結果): " + "{}".format(num1))
+                print("shift radix (hex):       " + "{}".format(num0))
+                print("hex right shift (result): " + "{}".format(num1))
             return int(num1, 0)
         else:
             if flag_print:
-                print("左シフトか右シフトかの指定がありません。\nやり直してください。")
+                print("There is no specification of left shift or right shift.\nPlease try again.")
     else:
         if flag_print:
-            print("基数が間違っています。\nやり直してください。")
+            print("The radix is incorrect.\nPlease try again.")
         
-#####################################################################
 """
-    ビットマスク
-    備考:
+    Bit Mask
+    details:
+    ・base: int (other than Decimal: 0b, 0o, 0x)
+    ・shift: int
+    ・radix: 2 | 8 | 10 | 16 (int)
+    ・lr: left | right (str)
+    ・flag_print=True: result print
+    ※Return int
         ・base(基数)とmask(マスク)への入力は自由でどの進数もint型(10進数はそのままでその他の進数は0b, 0o, 0x表記)
         ・operandへの入力はmaskに乗っ取ったマスクする進数でint型(operandは2進数か16進数しか選べない)(初期値: 16進数)
         ・flag_printがTrueの場合print出力する
