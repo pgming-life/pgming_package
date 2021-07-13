c12abdd8a2069919057b990916d11a67e134f071
Pgming <84230279+pgming-life@users.noreply.github.com>
2021-07-13 14:00:20 +0900

    Update comments

--- fwmodule_console_progress.py
+++ fwmodule_console_progress.py
@@ -5,29 +5,25 @@ flag_release = False
 try:
     import pgming_package.release as r
 except Exception:
-    # エラースルー
     pass
 else:
     flag_release = r.flag
 
-# 上記のインポートが読み込めない場合はflag_releaseは基本的にFalse
-if flag_release:    # リリース用インポート
+if flag_release:
     from pgming_package.fwmodule_general import *
-else:               # デバッグ用インポート
-    # ルートパス可変制御
-    import sys, os      # fwdef.pyと同じモジュールインポートだが、Python内部でインクルードガード出来ているので問題ない。
+else:
+    # variable route path control for VSCode debug
+    import sys, os
     if not [i for i in sys.path if i == os.path.dirname(__file__)]:
         sys.path.append(os.path.dirname(__file__))
     from fwmodule_general import *
 
-#####################################################################
 """
-    pythonスタート
-
+    Python Start
 """
 def python_start():
-    print("\n本当に実行させますか？")
-    print("このまま実行するにはEnterキーを押してください...")
+    print("\nDo you want to run it?")
+    print("\nExecutes when the Enter key is pressed...")
     print("    (  I        I  )")
     print("    (  I   II   I  )")
     print("   ((  I  I  I  I  ))")
@@ -47,11 +43,9 @@ def python_start():
     print("            ))")
     print("           ((")
 
-#####################################################################
 """
-    繋ぎ
-    備考: 計算結果を出力するときの繋ぎ
-
+    Python Connection
+    details: Connection at the time of calculation result output.
 """
 def python_connection():
     print("            ))")
@@ -59,52 +53,39 @@ def python_connection():
     print("            ))")
     print("           ((")
 
-#####################################################################
 """
-    サブ計算(非確定的処理)
-    備考: サブ計算を使うのはrangeの最大値が決まっていない(tqdmは使えない)とき。だからグルグル棒で表す。
-
+    Sub Calc (Nondeterministic processing)
+    details: When the maximum value of range is not decided, it is represented by a guruguru bar.
 """
 class python_sub:
-    # コンストラクタ
     def __init__(self, name_calc):
-        # インスタンス変数
         self.name_calc = name_calc
         self.g = guruguru()
-
-    # # インスタンスメソッド
-    # スタート(for文が始まる前の外側に挿入)
+        
     def start(self):
         print("{}...\r".format(self.name_calc), end="")
     
-    # 計算中(for文中に挿入)
     def calc(self):
-        # クラス変数
         print("{0}...{1}\r".format(self.name_calc, next(self.g)), end="")
-
-    # 終了処理
+        
     def end(self):
         print("{}...OK!!".format(self.name_calc))
 
-# グルグル棒ジェネレータ
+# guruguru bar
 def guruguru():
     i = 0
     while 1:
         yield "|/--/"[i % 5]
         i += 1
 
-#####################################################################
 """
-    メイン計算(確定的処理)
-    備考: メイン計算ではtqdmを扱う
+    Main Calc (Deterministic processing)
+    details: Use the tqdm.
+"""
+#for i in tqdm(range(n)):
 
 """
-#for i in tqdm(range(?)):      # for文にtqdmを掛ける
-
-#####################################################################
-"""
-    pythonエンド
-
+    Python End
 """
 def python_end():
     print("            ))")
@@ -114,47 +95,40 @@ def python_end():
     print("            ))")
     print("           ((")
     print("           )(")
-    input("\n処理が終了しました。ウィンドウを閉じるにはEnterを押してください...\n\n")
+    input("\nThe process is finished.\nCloses the window when the Enter key is pressed...\n\n")
     sys.exit()
 
-#####################################################################
 """
-    クラス型デコレータ
-    備考:
-    ・コンストラクタとデストラクタの機能があり、それぞれPythonスタートとPythonエンドを持つ
-    ・メイン関数に装飾することで使用可能
-
+    Class Decorator
+    details:
+    ・Has constructor and destructor functionality.
+    ・Use for the Main Function.
 """
-def class_decolater(func):
+def class_decorator(func):
     def wrapper():
-        python_start()  # Pythonスタート
-        func()          # メイン関数実行
-        python_end()    # Pythonエンド
+        python_start()
+        func()
+        python_end()
     return wrapper
 
-#####################################################################
 """
-    example
-    備考: 進捗テスト
-
+    Tests
 """
-# ↓↓↓直接このファイルを実行したときの処理
 if __name__ == "__main__":
     #from fwmodule_console_progress import *
 
-    # メイン関数
-    @class_decolater
+    # main function
+    @class_decorator
     def main_func():
-        # 繋ぎ
         for i in range(5):
-            print("出力結果の表示{}".format(i))
+            print("output result: " + i)
         python_connection()
         for i in range(5):
-            print("出力結果の表示{}".format(i))
+            print("output result: " + i)
         python_connection()
 
-        # サブ計算(非確定的処理)
-        sub = python_sub("サブ計算")
+        # Sub Calc
+        sub = python_sub("Sub Calc")
         sub.start()
         for i in range(100):
             sub.calc()
@@ -162,13 +136,13 @@ if __name__ == "__main__":
         sub.end()
         python_connection()
 
-        # メイン計算(確定的処理)
-        print("メイン計算")
+        # Main Calc
+        print("Main Calc")
         for i in tqdm(range(100)):
             time.sleep(.01)
         python_connection()
 
-        # 自作プログレスバー
+        # self-made progressbar
         for i in range(1, 21):
             sys.stdout.write("\r[ %-20s %3.0f%% ]" % ("#" * i, i * 100 / 20))
             sys.stdout.flush()
@@ -176,7 +150,7 @@ if __name__ == "__main__":
         print()
         python_connection()
 
-        # 自作プログレスキャラ
+        # self-made progress moving
         for i in range(1, 51):
             char = "|>-<"[i % 4] 
             sys.stdout.write("\r%s %3.0f%% done %s" % (char * 10, i * 100 / 50, char * 10))
@@ -184,10 +158,10 @@ if __name__ == "__main__":
             time.sleep(.1)
         print()
 
-    # メイン関数実行
+    # execute
     main_func()
 
-    # シャットダウン
+    # shutdown
     #import sys
     #sys.exit()
     
